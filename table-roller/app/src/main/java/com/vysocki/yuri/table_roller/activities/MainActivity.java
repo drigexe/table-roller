7b58057fcbfc2fa116ec67527c4b88970d23254e
Yuri Vysocki <drigexe@gmail.com>
2018-06-20 13:34:31 +0300

    Organizing packages by category

--- table-roller/app/src/main/java/com/vysocki/yuri/table_roller/activities/MainActivity.java
+++ table-roller/app/src/main/java/com/vysocki/yuri/table_roller/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.vysocki.yuri.table_roller;
+package com.vysocki.yuri.table_roller.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.vysocki.yuri.table_roller.R;
 
 public class MainActivity extends AppCompatActivity {
 
