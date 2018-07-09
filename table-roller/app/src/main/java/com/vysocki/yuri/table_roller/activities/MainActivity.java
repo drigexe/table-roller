735ddb7ee63c8b8f3fa5c458e63f29a0fb6cb474
Yuri Vysocki <drigexe@gmail.com>
2018-07-09 11:11:13 +0300

    connected Navigation Component with BottomNavigationView properly

--- table-roller/app/src/main/java/com/vysocki/yuri/table_roller/activities/MainActivity.java
+++ table-roller/app/src/main/java/com/vysocki/yuri/table_roller/activities/MainActivity.java
@@ -1,15 +1,14 @@
 package com.vysocki.yuri.table_roller.activities;
 
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.vysocki.yuri.table_roller.R;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,34 +20,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
         setNavController(R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNav, navController);
 
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                    switch (item.getItemId()) {
-                        case R.id.actionsFragment:
-                            navigateNavController(R.id.actionsFragment);
-                            break;
-
-                        case R.id.knowledgebaseFragment:
-                            navigateNavController(R.id.knowledgebaseFragment);
-                            break;
-
-                        case R.id.charactersFragment:
-                            navigateNavController(R.id.charactersFragment);
-                            break;
-                    }
-
-                    return true;
-                }
-            };
 
     public void setNavController(int hostFragment) {
         this.navController = Navigation.findNavController(this, hostFragment);
