d46fbc4ee60b038f6cf0fc5ed030ad1fef5a1d40
Yuri Vysocki <drigexe@gmail.com>
2018-07-12 16:15:47 +0300

    testing how Navigation component work with navigation.navigate and navControllers from fragments ouside the navGraph

--- table-roller/app/src/main/java/com/vysocki/yuri/table_roller/fragments/internal/KnowledgeListFragment.java
+++ table-roller/app/src/main/java/com/vysocki/yuri/table_roller/fragments/internal/KnowledgeListFragment.java
@@ -7,14 +7,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.vysocki.yuri.table_roller.R;
+import com.vysocki.yuri.table_roller.activities.MainActivity;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class KnowledgeListFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_knowledge_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_knowledge_list, container, false);
+
+        Button goButton = view.findViewById(R.id.gobutton);
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+                  navController.navigate(R.id.knowledgeDetail);
+            }
+        });
+
+        return view;
     }
+
 }
