2f934c44cd2a5866cb94013ad6a720ae076f32c4
Yuri Vysocki <drigexe@gmail.com>
2018-07-13 18:42:17 +0300

    created internal fragment for detailed information about particular fragment; changed particular knowledge fragment to be external and implement ExternalFragmentEstalisher interface

--- table-roller/app/src/main/java/com/vysocki/yuri/table_roller/fragments/external/KnowledgeParticularFragment.java
+++ table-roller/app/src/main/java/com/vysocki/yuri/table_roller/fragments/external/KnowledgeParticularFragment.java
@@ -4,16 +4,43 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.vysocki.yuri.table_roller.R;
+import com.vysocki.yuri.table_roller.fragments.internal.KnowledgeParticularInternalFragment;
+import com.vysocki.yuri.table_roller.interfaces.ExternalFragmentEstablisher;
 
-public class KnowledgeParticularFragment extends Fragment {
+public class KnowledgeParticularFragment extends Fragment implements ExternalFragmentEstablisher {
+
+    FragmentTransaction transaction;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_knowledge_particular, container, false);
+        View view = inflater.inflate(R.layout.fragment_external, container, false);
+
+        KnowledgeParticularInternalFragment particularFragment = new KnowledgeParticularInternalFragment();
+        setInternalFragment(R.id.container_top, particularFragment);
+
+        return view;
+    }
+
+    @Override
+    public void setInternalFragment(int frameLayoutId, Fragment internalFragment) {
+        transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(frameLayoutId, internalFragment).commit();
+    }
+
+    @Override
+    public boolean getTwoPaneMode(int configResourceTwoPaneMode) {
+        return getResources().getBoolean(configResourceTwoPaneMode);
+    }
+
+    @Override
+    public boolean getScreenSize(int configResourceTabletSize) {
+        return getResources().getBoolean(configResourceTabletSize);
     }
 }
