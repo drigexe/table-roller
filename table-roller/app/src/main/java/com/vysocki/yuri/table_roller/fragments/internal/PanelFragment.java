a9d59cd16ff2479151b9c0237977424a64614355
Yuri Vysocki <drigexe@gmail.com>
2018-07-18 17:04:59 +0300

    ImageViews and TextViews now can be setted at once

--- table-roller/app/src/main/java/com/vysocki/yuri/table_roller/fragments/internal/PanelFragment.java
+++ table-roller/app/src/main/java/com/vysocki/yuri/table_roller/fragments/internal/PanelFragment.java
@@ -7,17 +7,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.vysocki.yuri.table_roller.R;
 
 public class PanelFragment extends Fragment {
+
+    GridLayout grid;
+    int actionsCount;
+    TextView textView;
+    ImageView imageView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_panel, container, false);
 
+        grid = view.findViewById(R.id.panelGrid);
+        actionsCount = grid.getChildCount();
 
+        for (int i = 1; i <= actionsCount; i++ ) {
+            for (int y = -1; y >= -actionsCount; y--) {
+                imageView = view.findViewWithTag(Integer.toString(i));
+                textView = view.findViewWithTag(Integer.toString(y));
+
+                imageView.setImageResource(R.drawable.action_brain);
+                textView.setText("Some text");
+            }
+        }
 
         return view;
     }
